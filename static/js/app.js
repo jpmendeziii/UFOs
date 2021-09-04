7aaf49a071489e3cd00c8d94959a9a260bd34e99
jpmendeziii <jpmendeziii@gmail.com>
2021-09-04 09:39:13 -0500

    static/js/app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
 function buildTable(data) {
  // First, clear out any existing data
  tbody.html("");
